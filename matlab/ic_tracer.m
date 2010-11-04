8dc37b46ad7d7ce198a1e9bf162e321d76929215
Rahul Mahajan <rahulm@uw.edu>
2010-11-04 22:18:11 +0000

    ic_tracer.m - create tracer from theta. must be sure that the file that it reads theta is localized in theta and not phi.
    git-svn-id: https://svn.riouxsvn.com/sqgmodel/trunk@19 bde0f0f0-3b85-4903-a1d0-8e0114a44ce5

---
+++ matlab/ic_tracer.m
@@ -0,0 +1,43 @@
+%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+% <next few lines under version control, D O  N O T  E D I T>
+% $Date$
+% $Author$
+% $Revision$
+% $Id$
+%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+%
+%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+%  ic_tracer - create tracer field of unit magnitude 
+%              will write out tr_init.nc 
+%
+%  fname - name of the th_init.nc file to read from
+%
+%  ic_tracer('th_init.nc')
+%
+%- - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+function ic_tracer(fname)
+
+nc = netcdf(fname,'nowrite');
+thB = nc{'thetaB'}(:);
+thT = nc{'thetaT'}(:);
+close(nc);
+
+Nx = size(thB,2);
+Ny = size(thT,1);
+
+maxval = max(max(abs(thB)));
+trB = abs(thB ./ maxval);
+maxval = max(max(abs(thT)));
+trT = abs(thT ./ maxval);
+
+nc = netcdf('tr_init.nc','clobber');
+nc('nx') = Nx;
+nc('ny') = Ny);
+nc{'tracerB'} = ncfloat('ny','nx');
+nc{'tracerT'} = ncfloat('ny','nx');
+nc{'tracerB'}(:) = trB;
+nc{'tracerT'}(:) = trT;
+close(nc);
+
+return;
